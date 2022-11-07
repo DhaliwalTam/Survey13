eb6da789f1422f92bc75e3b3e967288a0dfee834
Siddharth Verma <vermasiddharth51@gmail.com>
2022-11-07 17:36:33 -0500

    (Siddharth Verma) added landing page to edit a survey. Implemented full functionality for get and post request to render and process edit page.

--- OneDrive/Desktop/Fall2022/Web Development/Survey13/app/controllers/surveys.controller.server.js
+++ OneDrive/Desktop/Fall2022/Web Development/Survey13/app/controllers/surveys.controller.server.js
@@ -50,3 +50,70 @@ export function ProcessSurveyCreatePage(req, res, next) {
         };
     })
 }
+
+export function DisplaySurveyEditPage(req, res, next) {
+    let id = req.params.id;
+
+    surveyModel.findById(id, (err, survey) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', {
+            title: 'Edit Survey',
+            page: 'surveys/edit',
+            survey: survey,
+            //displayName: UserDisplayName(req)
+        });
+    });
+
+
+}
+
+export function ProcessSurveyEditPage(req,res,next){
+    let id = req.params.id;
+
+    let updatedSurvey = surveyModel({
+        _id: req.body.id,
+        createdBy: req.body.createdBy,
+        template: "Multiple Choice",
+        title: req.body.title,
+        createdOn: req.body.createdOn,
+        expiry: req.body.expiringOn,
+        questions: [],
+        options: []
+    });
+
+    for(var i = 0; i < req.body.ques.length; i++){
+        if(req.body.ques[i] !== ""){
+            updatedSurvey.questions.push(req.body.ques[i]);
+        }
+
+        if(req.body.ques[i] == "undefined" || req.body.ques[i] == null ){
+            const index = updatedSurvey.questions.indexOf(req.body.ques[i]);
+            if (index > -1) {
+                updatedSurvey.questions.splice(index, 1);
+            }
+        }
+
+        if(req.body[`choices${i+1}`] !== ""){
+            updatedSurvey.options.push(req.body[`choices${i+1}`]);
+        }
+
+        if(req.body[`choices${i+1}`] == "undefined" || req.body[`choices${i+1}`] == null){
+            updatedSurvey.options[i] = [];
+        }
+    }
+    
+    surveyModel.updateOne({
+        _id: id
+    }, updatedSurvey, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/surveys/list');
+    })
+}
--- OneDrive/Desktop/Fall2022/Web Development/Survey13/app/routes/surveys.route.server.js
+++ OneDrive/Desktop/Fall2022/Web Development/Survey13/app/routes/surveys.route.server.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 //import { AuthGuard } from "../utils/index.js";
-import {DisplaySurveyList, DisplayCreateSurveyPage, ProcessSurveyCreatePage} from "../controllers/surveys.controller.server.js";
+import {DisplaySurveyList, DisplayCreateSurveyPage, ProcessSurveyCreatePage,DisplaySurveyEditPage, ProcessSurveyEditPage} from "../controllers/surveys.controller.server.js";
 
 const router = Router();
 
 router.get('/surveys/list', DisplaySurveyList);
 router.get('/surveys/create',DisplayCreateSurveyPage);
 router.post('/surveys/create', ProcessSurveyCreatePage);
+router.get('/surveys/edit/:id', DisplaySurveyEditPage);
+router.post('/surveys/edit/:id', ProcessSurveyEditPage);
+
 
 export default router;
