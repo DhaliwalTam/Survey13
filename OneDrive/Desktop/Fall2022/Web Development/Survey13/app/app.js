fc8c53db10ea6dbf0d7016c7cb3342be2397fd85
grauconejo13 <vanessaf.victorino@yahoo.com>
2022-11-08 00:55:14 -0600

    css - the header, added the site logo, and survey's create page

--- OneDrive/Desktop/Fall2022/Web Development/Survey13/app/app.js
+++ OneDrive/Desktop/Fall2022/Web Development/Survey13/app/app.js
@@ -2,13 +2,14 @@
 import express from 'express';
 import logger from 'morgan';
 import session from 'express-session';
-import bodyParser from 'body-parser';//get info from html forms
-import cookieParser from 'cookie-parser';//read cookies (needed for auth)
+import bodyParser from 'body-parser'; //get info from html forms
+import cookieParser from 'cookie-parser'; //read cookies (needed for auth)
 
 // ES modules fix dor _dirname
-import path, {dirname} from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(
+    import.meta.url));
 
 //import Mongoose module
 import mongoose from 'mongoose';
@@ -16,7 +17,7 @@ import mongoose from 'mongoose';
 // Auth Step 1 - import modules
 import passport from 'passport';
 import passportLocal from 'passport-local';
-import flash from 'connect-flash';//flash is a special area of the session used for storing messages. Messages are written to the flash and cleared after being displayed to the user.
+import flash from 'connect-flash'; //flash is a special area of the session used for storing messages. Messages are written to the flash and cleared after being displayed to the user.
 
 // Auth Step 2 - define our auth strategy
 let localStrategy = passportLocal.Strategy;
@@ -30,47 +31,47 @@ import surveysRouter from './routes/surveys.route.server.js'
 //import authRouter from './routes/auth.route.server.js';
 
 //configuration module + set up mongoose-complete db configuration
-import{Secret,MongoURI} from '../config/config.js'
+import { Secret, MongoURI } from '../config/config.js'
 
 //instantiate express application
 const app = express();
 
 //complete db configuration
 mongoose.connect(MongoURI);
-const db= mongoose.connection;
+const db = mongoose.connection;
 
 //listen for connection success or error
-db.on('open',()=> console.log("connected to mongoDb"));
-db.on('error',()=> console.log(" mongoDb connection error"));
+db.on('open', () => console.log("connected to mongoDb"));
+db.on('error', () => console.log(" mongoDb connection error"));
 
 //set up middleware
 
 
 //setup ViewEngine EJS
-app.set('views',path.join(__dirname,'/views'));
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'/client')));
-app.use(express.static(path.join(__dirname,'../public')));
+app.use(express.static(path.join(__dirname, '/client')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(session({
-    secret:Secret,
-    saveUninitialized:false,
-    resave:false
+    secret: Secret,
+    saveUninitialized: false,
+    resave: false
 }));
 
 // Auth Step 4 - Setup Express Session
 app.use(session({
     secret: Secret,
-    saveUninitialized: false, 
+    saveUninitialized: false,
     resave: false
 }));
 
 // Auth Step 5 -  Setup Flash
-app.use(flash());//use connect-flash for flash messages stored in session
+app.use(flash()); //use connect-flash for flash messages stored in session
 
 // Auth Step 6 - Initialize Passport and Session
 //a middle-ware that initialises Passport->initialises the authentication module. 
